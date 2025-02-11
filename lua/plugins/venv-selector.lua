41034b16fa56cef4b0b16d6ede121080b3551c21
Gabriel Romao <gabriel.romao@l5.com.br>
2025-02-11 16:36:22 -0300

    add venv-selector plugin

---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,10 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+  opts = {
+    dap_enabled = true,
+    -- Your options go here
+    -- name = "venv",
+    -- auto_refresh = false
+  },
+}
