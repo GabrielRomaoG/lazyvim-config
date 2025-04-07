eb966c486c5cb163b51dad166a8ed020575eccca
Gabriel Romao <gabriel-rgomes@hotmail.com>
2025-04-07 18:47:29 -0300

    remove latex from render-makrdown

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -6,15 +6,4 @@ return {
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {},
-  require("render-markdown").setup({
-    latex = {
-      enabled = true,
-      render_modes = false,
-      converter = "latex2text",
-      highlight = "RenderMarkdownMath",
-      position = "above",
-      top_pad = 0,
-      bottom_pad = 0,
-    },
-  }),
 }
