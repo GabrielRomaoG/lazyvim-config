aff7be0de9a08edb3220b71d0cbc320ba4c7358c
Gabriel Romao <gabriel-rgomes@hotmail.com>
2025-04-14 20:19:41 -0300

    add latex lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -27,6 +27,32 @@ return {
           client.server_capabilities.hoverProvider = false
         end,
       },
+      texlab = {
+        settings = {
+          texlab = {
+            bibtexFormatter = "texlab",
+            build = {
+              args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+              executable = "latexmk",
+              forwardSearchAfter = false,
+              onSave = true,
+            },
+            chktex = {
+              onEdit = false,
+              onOpenAndSave = false,
+            },
+            diagnosticsDelay = 300,
+            formatterLineLength = 80,
+            forwardSearch = {
+              args = {},
+            },
+            latexFormatter = "latexindent",
+            latexindent = {
+              modifyLineBreaks = true,
+            },
+          },
+        },
+      },
     },
   },
 }
