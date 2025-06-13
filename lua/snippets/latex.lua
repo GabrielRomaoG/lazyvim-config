0646cc0b66ae4981c10d5d9e726b615b03e625f8
Gabriel Romao <gabriel-rgomes@hotmail.com>
2025-06-13 14:17:22 -0300

    add vector lenght snippet

--- lua/snippets/latex.lua
+++ lua/snippets/latex.lua
@@ -14,15 +14,16 @@ local fmt = require("luasnip.extras.fmt").fmt
 local fmta = require("luasnip.extras.fmt").fmta
 
 return {
-  ls.add_snippets("tex", {
-    -- Begin question
-    s("bquestion", {
-      t("\\begin{question}{"),
-      i(1),
-      t("}{}"),
-      t({ "", "\t" }),
-      i(0),
-      t({ "", "\\end{question}" }),
+    ls.add_snippets("tex", {
+        -- Begin question
+        s("bquestion", {
+            t("\\begin{question}{"),
+            i(1),
+            t("}{}"),
+            t({ "", "\t" }),
+            i(0),
+            t({ "", "\\end{question}" }),
+        }),
+        s({ trig = "vlen", name = "Vector Lenght command" }, { t("\\vlen{"), i(1), t("}") }),
     }),
-  }),
 }
