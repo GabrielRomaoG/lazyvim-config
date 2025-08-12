783746eeb3167f15a3471200a6fa72ca2c8b1c24
Gabriel Romao <gabriel-rgomes@hotmail.com>
2025-08-12 18:09:29 -0300

    add math snippets to markdown

---
+++ lua/snippets/markdown.lua
@@ -0,0 +1,25 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+-- Inline math: $ ... $
+return {
+    ls.add_snippets("markdown", {
+        s("$", {
+            t({ "$", "\t" }),
+            i(1),
+            t({ "", "$" }),
+        }),
+        s("i$", {
+            t({ "$" }),
+            i(1),
+            t({ "$" }),
+        }),
+        s("$$", {
+            t({ "$$", "\t" }),
+            i(1),
+            t({ "", "$$" }),
+        }),
+    }),
+}
