713899811aa8aab8909e47d9413ea743c6b9dda3
Gabriel Romao <gabriel-rgomes@hotmail.com>
2025-08-11 21:16:36 -0300

    use latex snippets in markdowns

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,11 +1,13 @@
+local ls = require("luasnip")
 return {
     "L3MON4D3/LuaSnip",
     --   lazy = false,
     config = function()
         require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
-        require("luasnip").config.setup({
+        ls.config.setup({
             update_events = "TextChanged,TextChangedI",
             enable_autosnippets = true,
         })
+        ls.filetype_extend("markdown", { "tex" })
     end,
 }
